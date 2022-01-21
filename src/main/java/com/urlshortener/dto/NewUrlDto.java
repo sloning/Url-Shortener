5b72793713abc9d702591b5b5a4752c141abb089
sloning <i.96petrov@gmail.com>
2022-01-21 12:32:13 +0500

    dto url validation

--- src/main/java/com/urlshortener/dto/NewUrlDto.java
+++ src/main/java/com/urlshortener/dto/NewUrlDto.java
@@ -1,11 +1,13 @@
 package com.urlshortener.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.Date;
 
 @Data
 public class NewUrlDto {
+    @URL(message = "Invalid url")
     private String url;
     private boolean timeLess = true;
     private Date validUntil;
