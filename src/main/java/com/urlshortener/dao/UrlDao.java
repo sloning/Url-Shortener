cf6c1e5a53718c31d6fd1a238c19f32356e35eb8
sloning <i.96petrov@gmail.com>
2022-01-21 12:19:47 +0500

    url dao bug fixes and getByShortUrl method

--- src/main/java/com/urlshortener/dao/UrlDao.java
+++ src/main/java/com/urlshortener/dao/UrlDao.java
@@ -14,34 +14,49 @@ import java.util.Optional;
 public class UrlDao {
     private final JdbcTemplate jdbcTemplate;
 
-    public Optional<Url> get(String shortUrl) {
-        String sql = "select * from url where short_url = ?";
+    public Optional<Url> getByShortUrl(String url) {
+        String sql = "select * from url where short_url = ? and " +
+                "(expires_date > current_timestamp or expires_date is null)";
 
-        return jdbcTemplate.query(sql, ResultSetExtractorFactory.optionalExtractor(this::mapRowToUrl), shortUrl);
+        return jdbcTemplate.query(sql, ResultSetExtractorFactory.optionalExtractor(this::mapRowToUrl), url);
+    }
+
+    public Optional<Url> getByFullUrl(String url) {
+        String sql = "select * from url where url = ? and " +
+                "(expires_date > current_timestamp or expires_date is null)";
+
+        return jdbcTemplate.query(sql, ResultSetExtractorFactory.optionalExtractor(this::mapRowToUrl), url);
+    }
+
+    public long getNextId() {
+        String sql = "select nextval('url_id_seq')";
+
+        return jdbcTemplate.queryForObject(sql, Long.class);
     }
 
     public int save(Url url) {
-        String sql = "insert into url(url, short_url, valid_until, unique_visits, number_of_visits) " +
-                "values(?, ?, ?, ?, ?)";
+        String sql = "insert into url(id, url, short_url, expires_date, unique_visits, number_of_visits) " +
+                "values(?, ?, ?, ?, ?, ?)";
 
-        return jdbcTemplate.update(sql, url.getUrl(), url.getShortUrl(), url.getValidUntil(), url.getUniqueVisits(),
-                url.getNumberOfVisits());
+        return jdbcTemplate.update(sql, url.getId(), url.getUrl(), url.getShortUrl(), url.getExpiresDate(),
+                url.getUniqueVisits(), url.getNumberOfVisits());
     }
 
     public int update(Url url) {
-        String sql = "update url set unique_visits = ?, number_of_visits = ? where id = ?";
+        String sql = "update url set expires_date = ?, unique_visits = ?, number_of_visits = ? where id = ?";
 
-        return jdbcTemplate.update(sql, url.getUniqueVisits(), url.getNumberOfVisits(), url.getId());
+        return jdbcTemplate.update(sql, url.getExpiresDate(), url.getUniqueVisits(), url.getNumberOfVisits(),
+                url.getId());
     }
 
-    public int delete(Url url) {
+    public int delete(Long id) {
         String sql = "delete from url where id = ?";
 
-        return jdbcTemplate.update(sql, url.getId());
+        return jdbcTemplate.update(sql, id);
     }
 
-    public int deleteInvalidUrls() {
-        String sql = "delete from url where valid_until <= current_timestamp";
+    public int deleteExpiredUrls() {
+        String sql = "delete from url where expires_date <= current_timestamp";
 
         return jdbcTemplate.update(sql);
     }
@@ -52,7 +67,7 @@ public class UrlDao {
         url.setId(rs.getLong("id"));
         url.setUrl(rs.getString("url"));
         url.setShortUrl(rs.getString("short_url"));
-        url.setValidUntil(rs.getDate("valid_until"));
+        url.setExpiresDate(rs.getDate("expires_date"));
         url.setUniqueVisits(rs.getLong("unique_visits"));
         url.setNumberOfVisits(rs.getLong("number_of_visits"));
 
