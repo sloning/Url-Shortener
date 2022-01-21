fbf3ed4f42ab2d48c00005273578ddbe142a0394
sloning <i.96petrov@gmail.com>
2022-01-21 12:32:37 +0500

    error response dto

---
+++ src/main/java/com/urlshortener/dto/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.urlshortener.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private int code;
+    private String message;
+}
