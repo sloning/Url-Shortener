a1c712a103c8b4edf417da591cf3a3d444a783f3
sloning <i.96petrov@gmail.com>
2022-01-21 12:22:00 +0500

    jdbc config

---
+++ src/main/java/com/urlshortener/config/JdbcConfig.java
@@ -0,0 +1,24 @@
+package com.urlshortener.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+
+@Configuration
+public class JdbcConfig {
+    @Bean
+    public DataSource postgresqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        Map<String, String> env = System.getenv();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(env.get("db_host") + "url_shortener");
+        dataSource.setUsername(env.get("db_username"));
+        dataSource.setPassword(env.get("db_password"));
+
+        return dataSource;
+    }
+}
